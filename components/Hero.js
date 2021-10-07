18e34659b4abf30d5b3a5628f1e42810694c925e
hermancai <30858461+hermancai@users.noreply.github.com>
2021-10-06 19:22:13 -0700

    use heroicons

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,23 @@
+import { ChevronDoubleDownIcon } from "@heroicons/react/solid";
+
 function Hero({ id }) {
   return (
-    <div id={id} className="flex w-full items-center justify-center h-screen">
-      <p>Hello, I'm Herman.</p>
+    <div
+      id={id}
+      className="flex flex-col w-full items-center justify-center h-screen text-3xl px-10 text-center"
+    >
+      <p>
+        Hello, I'm <span className="text-red-500">Herman</span>.
+      </p>
+      <p>I am a full-stack developer.</p>
+      <ChevronDoubleDownIcon
+        className="w-8 mt-10 animate-bounce cursor-pointer text-red-500"
+        onClick={() => {
+          document
+            .getElementById("aboutWrapper")
+            .scrollIntoView({ behavior: "smooth" });
+        }}
+      />
     </div>
   );
 }
