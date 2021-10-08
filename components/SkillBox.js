7b9fefa804ca9aa0fbb58b32cac8e805babd4300
hermancai <30858461+hermancai@users.noreply.github.com>
2021-10-07 20:35:43 -0700

    remake skills table

--- components/SkillBox.js
+++ components/SkillBox.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 function SkillTable({ name, imgPath }) {
   return (
-    <div className="flex flex-col justify-center items-center rounded-md border-2 border-red-500 min-w-[150px] p-2 m-3 bg-gray-800">
-      <Image src={imgPath} height={50} width={50} />
-      {name}
+    <div className="flex space-x-2 rounded-md bg-gray-900 py-2 px-3 m-1 lg:m-2">
+      <Image src={imgPath} height={25} width={25} />
+      <p>{name}</p>
     </div>
   );
 }
