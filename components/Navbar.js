5a08f9c868f1d8fb6ef7c2bba53a7619ce518203
Herman Cai <hermancai3@gmail.com>
2021-12-14 20:44:32 -0800

    keep navbar always on top

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,39 @@
 function Navbar() {
   return (
-    <div className="flex fixed w-full bg-gray-900">
+    <div className="flex fixed w-full bg-gray-900 z-10">
       <div className="flex w-full justify-evenly py-5 sm:space-x-10 sm:justify-end md:space-x-16 sm:px-16">
-        <p>HOME</p>
-        <p>ABOUT</p>
-        <p>PROJECTS</p>
-        <p>CONTACT</p>
+        <p
+          className="navbarLink"
+          onClick={() => {
+            document.getElementById("hero").scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          HOME
+        </p>
+        <p
+          className="navbarLink"
+          onClick={() => {
+            document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          ABOUT
+        </p>
+        <p
+          className="navbarLink"
+          onClick={() => {
+            document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          PROJECTS
+        </p>
+        <p
+          className="navbarLink"
+          onClick={() => {
+            document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          CONTACT
+        </p>
       </div>
     </div>
   );
