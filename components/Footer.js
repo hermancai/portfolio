77c2fedc84967ebc7ce580b0cb99ff513bb5c548
Herman Cai <hermancai3@gmail.com>
2021-12-19 04:46:38 -0800

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+function Footer() {
+  return (
+    <div className="flex flex-wrap justify-between items-center w-full bg-gray-900 p-6">
+      <p className="font-light text-gray-500">&copy; {new Date().getFullYear()} Herman Cai</p>
+      <div className="flex flex-wrap gap-5">
+        <a href="https://www.linkedin.com/in/hermancai/" target="_blank" rel="noopener noreferrer">
+          <Image src="/images/In-White-96@2x.png" alt="linkedin logo" width={25} height={25} />
+        </a>
+        <a href="https://github.com/hermancai" target="_blank" rel="noopener noreferrer">
+          <Image src="/images/GitHub-Mark-Light-64px.png" alt="github logo" width={25} height={25} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,26 @@ import Navbar from "../components/Navbar";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div className="flex flex-col bg-gray-800 text-white">
       <Head>
         <title>Herman Cai's Portfolio</title>
-        <meta name="description" content="Herman Cai's portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
 
       <Navbar />
       <Hero id="hero" />
+      <span className="w-full h-[1px] bg-gray-900 bottom-0"></span>
       <About id="about" />
       <Projects id="projects" />
       <Contact id="contact" />
+      <span className="w-full h-[1px] bg-red-900 bottom-0"></span>
+      <Footer />
     </div>
   );
 }
