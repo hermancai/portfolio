5689e4eee58c80b2428683373bc110d05fb8d661
Herman Cai <hermancai3@gmail.com>
2021-12-19 04:48:49 -0800

    fill in about me section

--- components/About.js
+++ components/About.js
@@ -1,37 +1,48 @@
 function About({ id }) {
   return (
     <div id={id} className="flex w-full justify-center pt-[100px]">
-      <div className="flex flex-col space-y-16 md:space-y-0 md:flex-row w-4/5 justify-between">
-        <div className="w-full md:w-[45%] space-y-5">
+      <div className="flex flex-col space-y-[100px] md:space-y-0 md:flex-row w-4/5 justify-between">
+        <div className="flex flex-col w-full md:w-[45%] space-y-10">
           <p className="text-2xl underline underline-offset-8 decoration-4 decoration-red-500">About Me</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
+          <div className="bg-gray-900 p-8 rounded flex-1 leading-loose text-gray-100">
+            <p>
+              My name is <span className="text-red-500">Herman Cai</span>. I am a fullstack developer currently focused
+              on building with React and Node. I believe there is always more to learn, so I constantly strive to
+              develop new skills and improve my work. Please feel free to{" "}
+              <span
+                className="underline text-red-500 cursor-pointer"
+                onClick={() => {
+                  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                contact me
+              </span>{" "}
+              for any opportunities or inquiries.
+            </p>
+          </div>
         </div>
-        <div className="w-full md:w-[45%] space-y-5">
+        <div className="flex flex-col w-full md:w-[45%] space-y-10">
           <p className="text-2xl underline underline-offset-8 decoration-4 decoration-red-500">My Skills</p>
-          <div className="flex flex-col space-y-2">
-            <div className="flex flex-wrap gap-2">
-              <p className="skillBox">HTML</p>
-              <p className="skillBox">CSS</p>
-              <p className="skillBox">tailwindcss</p>
-              <p className="skillBox">Javascript</p>
-              <p className="skillBox">React</p>
-            </div>
-            <hr />
-            <div className="flex flex-wrap gap-2">
-              <p className="skillBox">Node</p>
-              <p className="skillBox">Express</p>
-              <p className="skillBox">PostgreSQL</p>
-            </div>
-            <hr />
-            <div className="flex flex-wrap gap-2">
-              <p className="skillBox">Github</p>
-              <p className="skillBox">VSCode</p>
+          <div className="bg-gray-900 p-8 rounded flex-1">
+            <div className="flex flex-col space-y-3">
+              <div className="flex flex-wrap gap-2">
+                <p className="skillBox">HTML</p>
+                <p className="skillBox">CSS</p>
+                <p className="skillBox">tailwindcss</p>
+                <p className="skillBox">Javascript</p>
+                <p className="skillBox">React</p>
+              </div>
+              <hr className="text-gray-500" />
+              <div className="flex flex-wrap gap-2">
+                <p className="skillBox">Node</p>
+                <p className="skillBox">Express</p>
+                <p className="skillBox">PostgreSQL</p>
+              </div>
+              <hr className="text-gray-500" />
+              <div className="flex flex-wrap gap-2">
+                <p className="skillBox">Github</p>
+                <p className="skillBox">VSCode</p>
+              </div>
             </div>
           </div>
         </div>
