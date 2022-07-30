e3f26351af8b7a24825a27d9dd155411432da471
Herman Cai <hermancai3@gmail.com>
2022-07-30 02:50:13 -0700

    Optimize background with next image

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 function Navbar({ inViewList, entryList }) {
   return (
-    <div className="flex flex-col fixed w-full bg-gray-900 z-10">
+    <div className="flex flex-col fixed w-full bg-gray-900 z-20">
       <div className="flex w-full justify-evenly py-5 sm:space-x-10 sm:justify-end md:space-x-16 sm:px-16">
         <p
           className={`navbarLink ${
